e9af8df0ee19222d2f9d3f7a4ca458859f292b1b
soumya <soumyapokale@gmail.com>
2024-07-21 16:38:25 +0530

    commit bankuserdetails database

--- src/main/java/com/BankingApplicationForSoumya/bank/entity/BankUsersdetails.java
+++ src/main/java/com/BankingApplicationForSoumya/bank/entity/BankUsersdetails.java
@@ -3,34 +3,84 @@ package com.BankingApplicationForSoumya.bank.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-import java.util.Date;
-
-@Entity @Table(name = "BankUsersetails")
+@Entity
+@Table(name = "bank_user_details")
 public class BankUsersdetails {
 
     @Id
+    @Column(name = "user_id")
     private String userId;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
-    private Date dateOfBirth;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+   @Column(name = "gender")
     private String gender;
-    private String street;
+
+    @Column(name = "street_address")
+    private String street;  // Renamed to avoid potential conflict
+
+    @Column(name = "city")
     private String city;
-    private String state;
+
+    @Column(name = "state_province")
+    private String state;  // Renamed to avoid potential conflict
+
+    @Column(name = "postal_code")
     private String postalCode;
+
+    @Column(name = "country")
     private String country;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
-    private String email;
+
+    @Column(name = "email_address")
+    private String email;  // Renamed to avoid potential conflict
+
+    @Column(name = "national_id")
     private String nationalId;
 
-    // Account Information
+    @Column(name = "account_number")
     private String accountNumber;
-    private String accountType;
-    private double accountBalance;
-    private String accountStatus;
-    private String currency;
 
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "account_balance")
+    private double accountBalance;
+
+    @Column(name = "account_status")
+    private String accountStatus;
+
+    @Column(name = "currency_type")
+    private String currency;  // Renamed to avoid potential conflict
+
+
+
+    @Column(name = "employer_name")
+    private String employerName;
+
+    @Column(name = "employment_status")
+    private String employmentStatus;
+
+    @Column(name = "annual_income")
+    private double annualIncome;
+
+    @Column(name = "job_title")
+    private String jobTitle;
+
+    // Getters and Setters
     public String getUserId() {
         return userId;
     }
@@ -54,172 +104,164 @@ public class BankUsersdetails {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNationalId() {
-        return nationalId;
-    }
-
-    public void setNationalId(String nationalId) {
-        this.nationalId = nationalId;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public double getAccountBalance() {
-        return accountBalance;
-    }
-
-    public void setAccountBalance(double accountBalance) {
-        this.accountBalance = accountBalance;
-    }
-
-    public String getAccountStatus() {
-        return accountStatus;
-    }
-
-    public void setAccountStatus(String accountStatus) {
-        this.accountStatus = accountStatus;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public Date getDateOpened() {
-        return dateOpened;
-    }
-
-    public void setDateOpened(Date dateOpened) {
-        this.dateOpened = dateOpened;
-    }
-
-    public String getEmployerName() {
-        return employerName;
-    }
-
-    public void setEmployerName(String employerName) {
-        this.employerName = employerName;
-    }
-
-    public String getEmploymentStatus() {
-        return employmentStatus;
-    }
-
-    public void setEmploymentStatus(String employmentStatus) {
-        this.employmentStatus = employmentStatus;
-    }
-
-    public double getAnnualIncome() {
-        return annualIncome;
-    }
-
-    public void setAnnualIncome(double annualIncome) {
-        this.annualIncome = annualIncome;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    private Date dateOpened;
-
-    // Employment Information
-    private String employerName;
-    private String employmentStatus;
-    private double annualIncome;
-    private String jobTitle;
+//
+//    public LocalDate getDateOfBirth() {
+//        return dateOfBirth;
+//    }
+//
+//    public void setDateOfBirth(LocalDate dateOfBirth) {
+//        this.dateOfBirth = dateOfBirth;
+//    }
+//
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+//
+//    public String getStreet() {
+//        return street;
+//    }
+//
+//    public void setStreet(String street) {
+//        this.street = street;
+//    }
+//
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//
+//    public String getState() {
+//        return state;
+//    }
+//
+//    public void setState(String state) {
+//        this.state = state;
+//    }
+//
+//    public String getPostalCode() {
+//        return postalCode;
+//    }
+//
+//    public void setPostalCode(String postalCode) {
+//        this.postalCode = postalCode;
+//    }
+//
+//    public String getCountry() {
+//        return country;
+//    }
+//
+//    public void setCountry(String country) {
+//        this.country = country;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getNationalId() {
+//        return nationalId;
+//    }
+//
+//    public void setNationalId(String nationalId) {
+//        this.nationalId = nationalId;
+//    }
+//
+//    public String getAccountNumber() {
+//        return accountNumber;
+//    }
+//
+//    public void setAccountNumber(String accountNumber) {
+//        this.accountNumber = accountNumber;
+//    }
+//
+//    public String getAccountType() {
+//        return accountType;
+//    }
+//
+//    public void setAccountType(String accountType) {
+//        this.accountType = accountType;
+//    }
+//
+//    public double getAccountBalance() {
+//        return accountBalance;
+//    }
+//
+//    public void setAccountBalance(double accountBalance) {
+//        this.accountBalance = accountBalance;
+//    }
+//
+//    public String getAccountStatus() {
+//        return accountStatus;
+//    }
+//
+//    public void setAccountStatus(String accountStatus) {
+//        this.accountStatus = accountStatus;
+//    }
+//
+//    public String getCurrency() {
+//        return currency;
+//    }
+//
+//    public void setCurrency(String currency) {
+//        this.currency = currency;
+//    }
+//
+//    public LocalDateTime getDateOpened() {
+//        return dateOpened;
+//    }
+//
+//    public void setDateOpened(LocalDateTime dateOpened) {
+//        this.dateOpened = dateOpened;
+//    }
+//
+//    public String getEmployerName() {
+//        return employerName;
+//    }
+//
+//    public void setEmployerName(String employerName) {
+//        this.employerName = employerName;
+//    }
+//
+//    public String getEmploymentStatus() {
+//        return employmentStatus;
+//    }
+//
+//    public void setEmploymentStatus(String employmentStatus) {
+//        this.employmentStatus = employmentStatus;
+//    }
+//
+//    public double getAnnualIncome() {
+//        return annualIncome;
+//    }
+//
+//    public void setAnnualIncome(double annualIncome) {
+//        this.annualIncome = annualIncome;
+//    }
+//
+//    public String getJobTitle() {
+//        return jobTitle;
+//    }
+//
+//    public void setJobTitle(String jobTitle) {
+//        this.jobTitle = jobTitle;
+//    }
 }
