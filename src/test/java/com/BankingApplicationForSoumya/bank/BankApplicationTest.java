bb19f3d282c2ecab1e853faa42a1dae14763c346
soumya <soumyapokale@gmail.com>
2024-07-21 15:52:14 +0530

    commit bankuserdetails repository

---
+++ src/test/java/com/BankingApplicationForSoumya/bank/BankApplicationTest.java
@@ -0,0 +1,12 @@
+package com.BankingApplicationForSoumya.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankApplicationTest {
+    @Test
+    void contextLoads() {
+        // This test will simply check if the application context can start
+    }
+}
