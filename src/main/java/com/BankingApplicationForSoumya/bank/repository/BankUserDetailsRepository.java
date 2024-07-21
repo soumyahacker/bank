5f8ad6674ffc1d96273b568994fa8322aeb8eda0
soumya <soumyapokale@gmail.com>
2024-07-21 15:36:53 +0530

    commit bankuserdetails repository

--- src/main/java/com/BankingApplicationForSoumya/bank/repository/BankUserDetailsRepository.java
+++ src/main/java/com/BankingApplicationForSoumya/bank/repository/BankUserDetailsRepository.java
@@ -1,7 +1,10 @@
 package com.BankingApplicationForSoumya.bank.repository;
 
+import com.BankingApplicationForSoumya.bank.entity.BankUsersdetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BankUserDetailsRepository extends JpaRepository {
+@Repository
+public interface BankUserDetailsRepository extends JpaRepository<BankUsersdetails ,Integer>{
 
 }
