775ce13d7fdf5e203a1f19d30f2853135bf8b5be
soumya <soumyapokale@gmail.com>
2024-08-30 22:47:50 +0530

    application prop deploy

--- src/main/java/com/BankingApplicationForSoumya/bank/BankApplication.java
+++ src/main/java/com/BankingApplicationForSoumya/bank/BankApplication.java
@@ -2,9 +2,16 @@ package com.BankingApplicationForSoumya.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankApplication {
+public class BankApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BankApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
